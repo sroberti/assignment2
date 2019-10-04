f18c6f583602fca1bad09fe207f19ba2f401079d
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-10-04 17:06:26 -0400

    Add Jet class implementation.
    Add implementations of functions to Jet.cpp.

--- Jet.cpp
+++ Jet.cpp
@@ -1,3 +1,40 @@
 //
 // Created by Samuel Roberti on 10/4/19.
 //
+#include "Jet.h"
+#include <random>
+#include <cmath>
+
+// Define constants
+#define MILEAGE_MIN 40
+#define MILEAGE_MAX 100
+
+int Jet::generateRandomizedMileage()
+{
+  // Using example code from
+  // http://www.cplusplus.com/reference/random/
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> dist(MILEAGE_MIN, MILEAGE_MAX);
+  return dist(generator);
+}
+
+Jet::Jet(string manufacturer, string model,
+         string fuelType, int engineCount)
+  : numberOfEngines{engineCount}
+{
+  setBrand(manufacturer);
+  setModel(model);
+  setFuelType(fuelType);
+}
+
+Jet::~Jet() = default;
+
+double Jet::mileageEstimate()
+{
+  if(fuelType=="Rocket" && numberOfEngines >= 2)
+    return std::floor(1.055 * mileage);
+  else return mileage;
+}
+
+
+
