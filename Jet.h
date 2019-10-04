ea2556eb68f9d57404ee1001a9bcb71f4b9767d4
Samuel Roberti <samuel.p.roberti@gmail.com>
2019-10-04 16:33:25 -0400

    Add interface for Jet class.
    Add content to header file for Jet class.

--- Jet.h
+++ Jet.h
@@ -1,3 +1,30 @@
 //
 // Created by Samuel Roberti on 10/4/19
 //
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+  const int numberOfEngines;
+  double mileage;
+
+  int generateRandomizedMileage();
+
+public:
+
+  Jet();
+  explicit Jet(string manufacturer, string model,
+               string fuelType, int engineCount=1);
+  virtual ~Jet();
+  virtual string toString();
+  virtual double mileageEstimate();
+
+};
+
+
+#endif 
